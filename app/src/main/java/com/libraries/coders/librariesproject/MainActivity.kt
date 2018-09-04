8abd07aa82fff831af97b8f8c8d3cab83c74fc22
Alvaro Falcón Morales <alvaro@2coders.com>
2018-09-04 14:56:07 +0100

    Added example

--- app/src/main/java/com/libraries/coders/librariesproject/MainActivity.kt
+++ app/src/main/java/com/libraries/coders/librariesproject/MainActivity.kt
@@ -2,11 +2,24 @@ package com.libraries.coders.librariesproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.libraries.coders.comcodersapiwrapper.API
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        API.initManager(basePath = "https://jsonplaceholder.typicode.com", baseHeaders = mapOf("Content-Type" to "application/json"), debug = true)
+        API.LOG_DEBUG_KEY = "Example"
+        API.request(
+                endPoint = "todos",
+                method = API.RequestMethod.GET,
+                onSuccess = { response ->
+                    println("DEBUG RESPONSE $response")
+                },
+                onFailure = { resultResponse, retryAction, statusCode ->
+                    println("DEBUG ERROR $resultResponse $statusCode")
+                }
+        )
     }
 }
